d4ac50ffc87ad90a86575bb952c8cdf6121895f7
Hua Jiang <huahua@email.unc.edu>
2022-10-03 15:08:11 -0400

    commit 1

--- server.js
+++ server.js
@@ -28,6 +28,30 @@
 // 2. set a header with content type `text/html`, and 
 // 3. end with the data that you are reading in from ./public/index.html.
 
+const http = require('http')
+const fs = require('fs')
+const hostname = '127.0.0.1'
+
+const minimist = require('minimist');
+var args = minimist(process.argv);
+const port = args.port || 3000;
+
+const server = http.createServer((req, res) => {
+    var data;
+        try {
+            data = fs.readFileSync('./public/index.html', 'utf8');
+            // console.log(data);
+          } catch (err) {
+            console.error(err);
+          }
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/html')
+    res.end(data)
+})
+
+server.listen(port, hostname, () => {
+  console.log(`Server listening on port ${port}`)
+})
 
 
 
